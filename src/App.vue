c99e5af099a45ac5f76303359ae1c191c8eb2d84
AndiZahiri <zahiriandii@gmail.com>
2024-11-27 22:59:15 +0100

    added NavBar and Hero component

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,11 @@
 <script setup>
     import navBar from '@/components/navBar.vue';
     import './assets/main.css';
+    import HeroPart from './components/heroPart.vue';
+    
 </script>
 
 <template>
     <navBar />
+    <HeroPart />
 </template>
