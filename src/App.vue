a04445288044f7b93a337766efeadb64027405ad
AndiZahiri <zahiriandii@gmail.com>
2024-11-29 17:46:19 +0100

    added the components in App.vue , imported PrimeIcons on project

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,13 @@
     import navBar from '@/components/navBar.vue';
     import './assets/main.css';
     import HeroPart from './components/heroPart.vue';
-    
+    import HomeCards from './components/HomeCards.vue';
+    import JobListings from './components/JobListings.vue';
 </script>
 
 <template>
     <navBar />
     <HeroPart />
+    <HomeCards />
+    <JobListings :limit="3"  showJobs="true"/>
 </template>
