993371b222402943d62e2d634f3517a92b59678c
AndiZahiri <zahiriandii@gmail.com>
2024-12-11 23:59:38 +0100

    created the seperate views

---
+++ src/views/HomeView.vue
@@ -0,0 +1,13 @@
+
+<script setup>
+    import heroPart from '@/components/heroPart.vue';
+    import HomeCards from '@/components/HomeCards.vue';
+    import JobListings from '@/components/JobListings.vue';
+</script>
+
+<template>
+
+    <HeroPart />
+    <HomeCards />
+    <JobListings :limit="3"  showJobs="true"/>
+</template>
