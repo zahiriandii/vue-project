db1e36015468beb744c74a75e827e8ef85ff0a31
AndiZahiri <zahiriandii@gmail.com>
2024-12-13 21:07:45 +0100

    added proxy '/api in the vite.config to access the server'

--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -20,7 +20,7 @@
 
     onMounted(async ()=>{
         try {
-            const response = await axios.get('http://localhost:5000/jobs');
+            const response = await axios.get('/api/jobs');
             state.jobs = response.data;
         } catch (error) {
             console.log("Error while fetching data",error);
