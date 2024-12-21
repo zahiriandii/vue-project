879a628408ce0fa7cfdb00633a74da30cd40c0bf
AndiZahiri <zahiriandii@gmail.com>
2024-12-21 18:43:36 +0100

    modified the signUp view to connect to the database

--- src/views/SignUpView.vue
+++ src/views/SignUpView.vue
@@ -1,6 +1,25 @@
 <script setup>
   import { RouterLink } from 'vue-router';
+  import { reactive } from 'vue';
+  import axios from 'axios';
 
+  const userData = reactive({
+    email: '',
+    password: ''
+  });
+
+  const registerUser = async () =>
+  {
+    try {
+      const response = await axios.post("http://localhost:5000/api/auth/register",{
+        email: userData.email,
+        password: userData.password
+      });
+      console.log("User registered ");
+    } catch (error) {
+      console.log(error);
+    }
+  }
 </script>
 
 <template>
@@ -17,7 +36,7 @@
   </div>
 
   <!-- Sign Up Form -->
-  <form>
+  <form @submit.prevent="registerUser">
     <!-- Name Field -->
     <div class="mb-4">
       <label for="name" class="block text-gray-600 font-medium mb-2">Full Name</label>
@@ -28,14 +47,14 @@
     <!-- Email Field -->
     <div class="mb-4">
       <label for="email" class="block text-gray-600 font-medium mb-2">Email</label>
-      <input type="email" id="email" placeholder="Enter your email"
+      <input v-model="userData.email" type="email" id="email" placeholder="Enter your email"
         class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400" />
     </div>
 
     <!-- Password Field -->
     <div class="mb-6 relative">
       <label for="password" class="block text-gray-600 font-medium mb-2">Password</label>
-      <input type="password" id="password" placeholder="Enter your password"
+      <input v-model="userData.password" type="password" id="password" placeholder="Enter your password"
         class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400" />
       <span class="absolute inset-y-0 right-3 flex items-center text-gray-400 cursor-pointer">
         👁️
